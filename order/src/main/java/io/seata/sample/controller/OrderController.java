43dadeb5b5c89c22fa8446fec449b8992abcf5a2
lilun <lilun@offerxiansheng.com>
2020-04-30 15:26:54 +0800

    修改order 测试feign

--- order/src/main/java/io/seata/sample/controller/OrderController.java
+++ order/src/main/java/io/seata/sample/controller/OrderController.java
@@ -1,9 +1,12 @@
 package io.seata.sample.controller;
 
-import io.seata.sample.service.OrderService;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.seata.sample.service.OrderService;
 
 /**
  * @author jimin.jm@alibaba-inc.com
@@ -13,14 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-    @GetMapping(value = "/create", produces = "application/json")
-    public Boolean create(String userId, String commodityCode, Integer count) {
+	@GetMapping(value = "/create", produces = "application/json")
+	public Boolean create(String userId, String commodityCode, Integer count) throws InterruptedException {
 
-        orderService.create(userId, commodityCode, count);
-        return true;
-    }
+        Thread.sleep(3000);
+		 
+		//int i = 1/0;
+		System.out.println("调用了订单服务 userId=" + userId + ",commodityCode = " + commodityCode);
+		// orderService.create(userId, commodityCode, count);
+		return true;
+	}
 
 }
